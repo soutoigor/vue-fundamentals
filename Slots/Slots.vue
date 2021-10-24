68c062bbf36e2156cb92ca083b0c2cfe92e1ee01
Igor Souto <igor_souto@outlook.com>
2021-10-24 20:54:50 -0300

    feat(examples): add slots examples

---
+++ Slots/Slots.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="card">
+    <header class="card__header">
+      <h1>
+        <slot name="header">
+          Default card Title
+        </slot>
+      </h1>
+    </header>
+    <main class="card__content">
+      <slot />
+    </main>
+    <footer class="card__footer">
+      <slot name="footer" :anything="anyMethod" />
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    anyMethod() {
+      alert('Alert!!! :D')
+    },
+  },
+}
+</script>
+
+<style>
+.card {
+  border: 1px solid #000;
+  padding: 2px;
+  border-radius: 6px;
+}
+
+.card__header {
+  font-style: italic;
+}
+
+.card__content {
+  font-size: .9rem;
+  font-weight: 600;
+}
+
+.card__footer {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
---
+++ Slots/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <slots-example>
+    This is the component's content.
+    <template #footer="{ anything }" }>
+      <button>Cancel</button>
+      <button @click="mySubmitMethod(anything)">Send</button>
+    </template>
+  </slots-example>  
+</template>
+
+<script>
+import SlotsExample from './Slots.vue'
+
+export default {
+  components: {
+    SlotsExample,
+  },
+  methods: {
+    mySubmitMethod(andThen) {
+      alert('Do somethings first')
+      andThen()
+    },
+  },
+}
+</script>
